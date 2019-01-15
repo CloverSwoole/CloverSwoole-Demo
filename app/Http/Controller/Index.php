28917eb25910a4a716af536607e2156206117c0f
戒尺 <itxiao6@qq.com>
2019-01-15 11:07:24 +0800

    测试控制器 demo

--- app/Http/Controller/Index.php
+++ app/Http/Controller/Index.php
@@ -1,7 +1,5 @@
 <?php
 namespace App\Http\Controller;
-
-
 use Itxiao6\Framework\Facade\Http\Abstracts\Controller;
 
 /**
@@ -14,11 +12,8 @@ class Index extends Controller
 
     function index()
     {
-        $this -> response -> getRawResponse() -> header('Content-type','application/json;charset=utf-8');
-//        $this -> response -> endResponse();
 //        $res = \App\Models\Users::take(1) -> get();
         $res = [1=>2];
         $this->ReturnJosn($res);
-//        $this -> response -> endResponse();
     }
 }
