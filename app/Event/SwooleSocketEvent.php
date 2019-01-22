66b514d48f40ec3bad5dbb9fc42aadde174562f1
戒尺 <itxiao6@qq.com>
2019-01-22 15:33:46 +0800

    Socket 时间模型demo 完善

--- app/Event/SwooleSocketEvent.php
+++ app/Event/SwooleSocketEvent.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Event;
+use CloverSwoole\CloverSwoole\Facade\SwooleSocket\Exception\Exception;
 use CloverSwoole\CloverSwoole\Facade\SwooleSocket\ServerEventInterface;
 
 /**
@@ -17,7 +18,14 @@ class SwooleSocketEvent implements ServerEventInterface
      */
     public function onClose($server,$fd)
     {
-        echo "Server Closed\n";
+        try{
+            echo "连接关闭:{$fd} \n";
+        }catch (\Throwable $exception){
+            /**
+             * 处理异常
+             */
+            echo "异常:{$exception -> getMessage()}\n";
+        }
     }
 
     /**
@@ -28,7 +36,11 @@ class SwooleSocketEvent implements ServerEventInterface
      */
     public function onOpen(\swoole_websocket_server $server, \swoole_http_request $request)
     {
-        echo "opOpened\n";
+        try{
+            echo "连接到达\n";
+        }catch (\Throwable $exception){
+            echo "异常:{$exception -> getMessage()}\n";
+        }
     }
 
     /**
@@ -39,7 +51,58 @@ class SwooleSocketEvent implements ServerEventInterface
      */
     public function onMessage(\swoole_websocket_server $server, \swoole_websocket_frame $frame)
     {
-        echo "onMessageEd\n";
+        try{
+            /**
+             * 判断信息是否正确
+             */
+            if(!($frame -> opcode == 1 && $frame -> finish == true && strlen($frame -> data) > 0)){
+                throw new Exception('数据非法');
+            }
+            /**
+             * 格式化数据
+             */
+            $data = json_decode($frame -> data,1);
+            /**
+             * 应用名过滤
+             */
+            if((!isset($data['app'])) || strlen($data['app']) < 1){
+                $data['app'] = 'Index';
+//                throw new Exception('应用不存在');
+            }
+            /**
+             * 模块过滤
+             */
+            if((!isset($data['controller'])) || strlen($data['controller']) < 1){
+                $data['controller'] = 'Index';
+//                throw new Exception('模块不存在');
+            }
+            /**
+             * 操作不存在
+             */
+            if((!isset($data['action'])) || strlen($data['action']) < 1){
+                $data['action'] = 'index';
+//                throw new Exception('操作不存在');
+            }
+            /**
+             * 获取消息的操作目的
+             */
+            $class = '\App\Socket\\'.$data['app'].'\\'.$data['controller'];
+            /**
+             * 判断控制器是否存在
+             */
+            if(!class_exists($class)){
+                throw new Exception('找不到指定控制器:'.$class);
+            }
+            /**
+             * 实例化控制器
+             */
+            (new $class($server,$frame)) -> __hook($data['action']);
+        }catch (\Throwable $exception){
+            /**
+             * 处理异常
+             */
+            echo "异常:{$exception -> getMessage()}\n";
+        }
     }
 
     /**
@@ -50,7 +113,7 @@ class SwooleSocketEvent implements ServerEventInterface
      */
     public function onRequest(\swoole_http_request $request, \swoole_http_response $response)
     {
-        echo "onRequestEd\n";
+        echo "WebAndSocketOnRequestEd\n";
     }
 
     /**
@@ -60,7 +123,7 @@ class SwooleSocketEvent implements ServerEventInterface
      */
     public function onShutdown(\swoole_websocket_server $server)
     {
-        echo "onShutdownEd\n";
+        echo "WebAndSocketOnShutdownEd\n";
     }
 
     /**
@@ -70,6 +133,6 @@ class SwooleSocketEvent implements ServerEventInterface
      */
     public function onStart(\swoole_websocket_server $server)
     {
-        echo "onStarted\n";
+        echo "WebAndSocketOnStarted\n";
     }
 }
